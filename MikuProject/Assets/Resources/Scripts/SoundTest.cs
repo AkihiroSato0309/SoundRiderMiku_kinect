a1ccd783bfacec5a23fbf79a55f1a300388f9b26
Keisuke Ohe <oh.keisu@gmail.com>
2015-07-27 14:37:10 +0900

    ジャンプ効果音を一旦別のデータに変更。

---
+++ MikuProject/Assets/Resources/Scripts/SoundTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTest : MonoBehaviour {
+
+	[SerializeField]
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			audio.time = 0.0f;
+			audio.Play();
+			print ("test");
+		}
+	}
+}
