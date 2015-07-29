a9ba2bda14332420b2ee85d061b57e8b7a6a63ee
Keisuke Ohe <oh.keisu@gmail.com>
2015-07-29 15:34:51 +0900

    WallManagerにコメントを追加。

--- MikuProject/Assets/Resources/Scripts/Stage/WallManager.cs
+++ MikuProject/Assets/Resources/Scripts/Stage/WallManager.cs
@@ -11,26 +11,31 @@ using System.Collections;
 using System.Collections.Generic;
 
 
+/***********************************************************************************************//**
+
+壁管理クラス. 壁を生成し, 常にプレイヤーの左右に壁が来るように座標を管理する.
+	
+***************************************************************************************************/
 public class WallManager : SingletonMonoBehaviour<WallManager>
 {
 	// --------------- inspector ---------------
 	[SerializeField]
-	GameObject wallPrefab;
+	GameObject wallPrefab;						// 生成したい壁のプレハブ
 	[SerializeField]
-	int num;
+	int num;									// 使い回す壁の数（多いほど遠方まで壁が設置される）
 	[SerializeField]
-	Vector3 offset = new Vector3 (10, 0, 10);
+	Vector3 offset = new Vector3 (10, 0, 10);	// 初期配置・ワープ時のオフセット. xは道幅の1/2, yは0, zは壁のz方向の長さ（奥行き）を入れるといい.
 	[SerializeField]
-	float warpStartDistanceZ;
+	float warpStartDistance;					// ワープを開始する基準となる距離（プレイヤーの背後からどれだけ離れたかでワープを判定する）
 
 	// --------------- property ---------------
-	public float RoadWidth { get { return offset.x * 2; } }
+	public float RoadWidth { get { return offset.x * 2; } }		// 形成される道幅 = 左壁と右壁間におけるX方向距離
 
 	// --------------- private ---------------
-	List<GameObject> leftWalls;
-	List<GameObject> rightWalls;
-	int backmostWallIndex = 0;
-	Transform playerTransform;
+	List<GameObject> leftWalls;		// 左の壁
+	List<GameObject> rightWalls;	// 右の壁
+	int backmostWallIndex = 0;		// 最後方にある壁を指すインデックス
+	Transform playerTransform;		// プレイヤーのTransform
 
 
 	/************************************************************************************//**
@@ -40,24 +45,26 @@ public class WallManager : SingletonMonoBehaviour<WallManager>
 	****************************************************************************************/
 	void Awake ()
 	{
-		// シングルトンになるように削除を行う
+		// シングルトンになるように削除を行う.
 		if (base.MakeIntoSingleton ()) return;
 
-		// プレイヤーの取得
+		// プレイヤーの取得. ワープ判定に使う.
 		this.playerTransform = GameObject.FindWithTag ("Player").transform;
 
-		// 壁生成準備
+		// 壁生成準備.
 		this.leftWalls = new List<GameObject> (this.num);
 		this.rightWalls = new List<GameObject> (this.num);
 
-		// 壁を生成
+		// 壁を生成.
 		for (int i = 0; i < this.num; i++)
 		{
+			// 左の壁.
 			GameObject wall = Instantiate (this.wallPrefab);
 			wall.transform.position = new Vector3 (-this.offset.x, this.offset.y, offset.z * i);
 			wall.transform.parent = this.transform;
 			this.leftWalls.Add (wall);
 
+			// 右の壁.
 			wall = Instantiate (this.wallPrefab);
 			wall.transform.position = new Vector3 (this.offset.x, this.offset.y, offset.z * i);
 			wall.transform.parent = this.transform;
@@ -72,17 +79,22 @@ public class WallManager : SingletonMonoBehaviour<WallManager>
 	****************************************************************************************/
 	void Update ()
 	{
+		// 最後方の左右の壁を取得.
 		GameObject leftWall = this.leftWalls[this.backmostWallIndex];
 		GameObject rightWall = this.rightWalls[this.backmostWallIndex];
-		float distance = this.playerTransform.position.z - leftWall.transform.position.z;
 
-		if (distance > this.warpStartDistanceZ)
+		// もし壁がプレイヤーの後方に行っていたら, 前方へワープさせる.
+		float distance = this.playerTransform.position.z - leftWall.transform.position.z;
+		if (distance > this.warpStartDistance)
 		{
-			float x = leftWall.transform.position.x;
-			float y = leftWall.transform.position.y;
+			// 左右の壁の座標を更新.
+			float x = this.offset.x;
+			float y = this.offset.y;
 			float z = leftWall.transform.position.z + this.offset.z * this.num;
 			leftWall.transform.position = new Vector3 (x, y, z);
 			rightWall.transform.position = new Vector3 (-x, y, z);
+
+			// 最後方の壁を指すインデックスを更新.
 			this.backmostWallIndex = ++this.backmostWallIndex % this.num;
 		}
 	}
