5aef689bc0e987469257a31231e869061760ea6d
unknown <s132080@GAME1309.tsitcl.local>
2015-07-30 19:57:48 +0900

    プレイヤーのプレハブを更新

---
+++ MikuProject/Assets/Resources/Particles/Explosions Particle Effect/readme.txt
@@ -0,0 +1,3 @@
+The Explosion and RingBoom uses the default particle material.
+The Debris uses the bubble material.
+Smoke Trail and Sparks uses the sparks material
---
+++ MikuProject/Assets/Resources/Scripts/EnemyAI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyAI : MonoBehaviour {
+
+	[SerializeField]
+	private float waitTime = 3.0f;
+	[SerializeField]
+	private float waitSpace = 10.0f;
+	[SerializeField]
+	private float speed = 0.3f;
+
+	private Vector3 basePos;
+	private Action updateFunc;
+
+	// Use this for initialization
+	void Start () 
+	{
+		updateFunc = InterUpdate;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		updateFunc ();
+		Debug.Log (SoundManager.Inst.DeltaTime);
+	}
+
+	void InterUpdate()
+	{
+		// 移動
+		transform.Translate (0.0f, 0.0f, -speed);
+		
+		Vector3 currentPos = transform.position;
+		basePos = Camera.main.transform.position;
+		float worldWatePosZ = basePos.z + waitSpace;
+		if (currentPos.z < worldWatePosZ) 
+		{
+			currentPos.z = worldWatePosZ;
+			transform.position = currentPos;
+			updateFunc = WaitUpdate;
+		}
+	}
+
+	void WaitUpdate()
+	{
+		Vector3 basePos = Camera.main.transform.position;
+		//Vector3 basePos = GameObject.FindWithTag("Player").transform.position;
+		Vector3 currentPos = transform.position;
+		currentPos.z = basePos.z + waitSpace;
+		// プレイヤーと並走する
+		transform.position = currentPos;
+
+		// 時間を減らす
+		waitTime -= Time.deltaTime;
+		if (waitTime < 0.0f)
+			updateFunc = RemoveUpdate;
+	}
+
+	void RemoveUpdate()
+	{
+		// 移動
+		transform.Translate (0.0f, 0.0f, -speed);
+	}
+}
